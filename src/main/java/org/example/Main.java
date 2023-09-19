d9a80a6bdc9eeed178062e46c94a56aad629e2ab
Your Name <you@example.com>
2023-09-20 00:43:09 +0300

    change 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello world!");
 
+        System.out.println("Another hello from yet another planet!");
+
+        Thread.sleep(1000);
+        System.exit(0);
     }
 }

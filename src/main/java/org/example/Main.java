f80736d6bdb6322a66909d2af657e2feca83aa49
Your Name <you@example.com>
2023-09-20 01:07:19 +0300

    change 10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+        Long counter = 0L;
         System.out.println("Hello world!");
 
         Thread.sleep(6000);
 
         new Thread(() -> {
-            System.out.println("3 + 3 == 9");
+            System.out.println("6 + 6 == 36");
         }).run();
     }
 }

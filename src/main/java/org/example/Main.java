5062a42118f1ea9766ffa905b79a9c40b507c109
Your Name <you@example.com>
2023-09-20 00:55:39 +0300

    change 5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello world!");
 
-        System.out.println("Another hello from Mercury!");
-
         Thread.sleep(6000);
         System.exit(1);
+
+        new Thread().run();
     }
 }
